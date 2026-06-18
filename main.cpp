4953ac302e9bec6cb7f568197b74e66866869601
unknown <sikafredrick5@gmail.com>
2026-06-18 23:20:48 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {cout << "Welcome To Snowflake's ATM!" << endl;
+    double balance = 1000.00;
+cout << "Your balance is: " << balance <<endl;
+
+    return 0;
+} 
+
+
