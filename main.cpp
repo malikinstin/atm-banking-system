89cddbcc8b6b5df04bbd80f107e3dde00575b651
unknown <sikafredrick5@gmail.com>
2026-06-21 23:58:34 +0000

    Implemented a Withdrawal Feature

--- main.cpp
+++ main.cpp
@@ -6,7 +6,25 @@ int main() {cout << "Welcome To Snowflake's ATM!" << endl;
     double balance = 1000.00;
 cout << "Your balance is: " << balance <<endl;
 
-    return 0;
+    double withdrawalAmount;
+cout << "How much do you want to withdraw"<<endl;
+cin >> withdrawalAmount;
+if (withdrawalAmount > balance || withdrawalAmount < 0)
+{
+   cout << "Insufficient funds"<<endl;
+   balance = balance;
+   cout << "Balance is: "<< balance <<endl;
+}
+else
+{
+    balance = balance - withdrawalAmount;
+    cout <<"Balance is: "<< balance << endl;
+    cout << "Withdrawal Successful"<<endl;
+}
+
+
+return 0;
+
 } 
 
 
