83a18ad17220a4642637dd4d9a0768f304934e4c
unknown <sikafredrick5@gmail.com>
2026-06-24 19:27:12 +0000

    Implemented a deposit feature

--- main.cpp
+++ main.cpp
@@ -9,17 +9,31 @@ cout << "Your balance is: " << balance <<endl;
     double withdrawalAmount;
 cout << "How much do you want to withdraw"<<endl;
 cin >> withdrawalAmount;
-if (withdrawalAmount > balance || withdrawalAmount < 0)
+if (withdrawalAmount > balance)
 {
    cout << "Insufficient funds"<<endl;
-   balance = balance;
-   cout << "Balance is: "<< balance <<endl;
+}
+else if (withdrawalAmount < 0)
+{
+cout << "Invalid withdrawal Amount" <<endl;
 }
 else
 {
     balance = balance - withdrawalAmount;
     cout <<"Balance is: "<< balance << endl;
     cout << "Withdrawal Successful"<<endl;
+}
+
+
+double depositAmount;
+cout << "How much do you want to deposit "<<endl;
+cin >>depositAmount;
+if (depositAmount < 0)
+{
+    cout << "Invalid Deposit Amount" << endl;
+} else {
+    balance = balance + depositAmount;
+    cout << "Your balance is "<< balance << endl;
 }
 
 
