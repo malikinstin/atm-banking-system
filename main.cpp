b192c4e8416e2786e10edc9ebc42ba4eb0090ac4
unknown <sikafredrick5@gmail.com>
2026-06-26 23:45:34 +0000

    Added a menu

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-int main() {cout << "Welcome To Snowflake's ATM!" << endl;
+/*int main() {cout << "Welcome To Snowflake's ATM!" << endl;
     double balance = 1000.00;
 cout << "Your balance is: " << balance <<endl;
 
@@ -39,6 +39,32 @@ if (depositAmount < 0)
 
 return 0;
 
-} 
+}*/
+
+int main() {
+    double balance = 1000.00;
+    bool running = true;
+
+    while (running) {
+        cout << "\n--- Snowflakes ATM ---" << endl;
+        cout << "1. Check Balance" << endl;
+        cout << "Enter your choice: " << endl;
+
+        int choice;
+
+    cin >> choice;
+
+    if (choice == 1) {
+        cout << "Your balance is: " << balance << endl;
+    } else if ( choice == 2) {
+        cout << " Thank you for using Snowflake's ATM. Goodbye!" << endl;
+        running == false;
+    }
+    else {
+        cout << " Invalid choice. Please try again."<< endl;
+         }
+    }
+    return 0;
+}
 
 
